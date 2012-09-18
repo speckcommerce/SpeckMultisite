0f662a1f7b32d3c7f666ebea01b4e82a94fddc14
nigel lundsten <nigel. lundsten@gmail.com>
2012-09-18 12:29:12 -0700

    pkey on website_id

--- data/schema.sql
+++ data/schema.sql
@@ -1,4 +1,5 @@
 CREATE TABLE `website` (
  `website_id` int(11) NOT NULL,
- `name` varchar(255) NOT NULL
+ `name` varchar(255) NOT NULL,
+ PRIMARY KEY (`website_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
