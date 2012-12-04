de6d10712d6083b0192863369b23c3b887e13b30
Aleksey Khudyakov <aleksey@xerkus.pro>
2012-12-05 04:22:03 +1100

    change default rdbms table encoding

--- data/schema.sql
+++ data/schema.sql
@@ -2,4 +2,4 @@ CREATE TABLE `website` (
  `website_id` int(11) NOT NULL,
  `name` varchar(255) NOT NULL,
  PRIMARY KEY (`website_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
